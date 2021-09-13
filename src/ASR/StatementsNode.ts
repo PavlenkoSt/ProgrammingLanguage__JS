6fd19e4c1c3d84e2b9af773a318b78c0c5cae78d
Stanislav <verbatim8872@gmail.com>
2021-09-13 16:47:29 +0300

    asr complete, need add run function to parser

--- src/ASR/StatementsNode.ts
+++ src/ASR/StatementsNode.ts
@@ -3,7 +3,7 @@ import ExpressionNode from "./ExpressionNode";
 export default class StatementsNode extends ExpressionNode {
     codeStrings: ExpressionNode[] = [];
 
-    addCode(node: ExpressionNode[]){
+    addCode(node: ExpressionNode){
         this.codeStrings.push(node);
     }
 }
--- src/Parser.ts
+++ src/Parser.ts
@@ -1,5 +1,11 @@
+import BinOperatorNode from "./ASR/BinOperatorNode";
+import ExpressionNode from "./ASR/ExpressionNode";
+import NumberNode from "./ASR/NumberNode";
+import StatementsNode from "./ASR/StatementsNode";
+import UnOperatorNode from "./ASR/UnOperatorNode";
+import VariableNode from "./ASR/VariableNode";
 import Token from "./Token";
-import TokenType from "./TokenType";
+import TokenType, { TokenTypeList } from "./TokenType";
 
 export default class Parser {
     tokens: Token[];
@@ -28,4 +34,81 @@ export default class Parser {
         }
         return token;
     }
+
+    parseVariableOrNumber(): ExpressionNode{
+        const variable = this.match(TokenTypeList.VARIABLE);
+        if(variable !== null){
+            return new VariableNode(variable);
+        }
+        
+        const number = this.match(TokenTypeList.NUMBER);
+        if(number !== null){
+            return new NumberNode(number);
+        }
+
+        throw new Error(`Ожидается переменная или число на позиции ${this.position}`)
+    }
+
+    parsePrint(): ExpressionNode{
+        const operatorLog = this.match(TokenTypeList.LOG);
+        if(operatorLog !== null){
+            return new UnOperatorNode(operatorLog, this.parseFormula());
+        }
+
+        throw new Error(`Ожидается унарный оператор ЛОГ на позиции ${this.position} позиции`);
+    }
+
+    parseParenthases(): ExpressionNode{
+        if(this.match(TokenTypeList.LPAR) !== null){
+            const node = this.parseFormula();
+            this.require(TokenTypeList.RPAR);
+            return node;
+        }else{
+            return this.parseVariableOrNumber();    
+        }
+    }
+
+    parseFormula(): ExpressionNode{
+        let leftNode = this.parseParenthases();
+        let operator = this.match(TokenTypeList.MINUS, TokenTypeList.PLUS);
+        while(operator !== null){
+            const rightNode = this.parseParenthases();
+            leftNode = new BinOperatorNode(operator, leftNode, rightNode);
+            operator = this.match(TokenTypeList.MINUS, TokenTypeList.PLUS);
+        }
+        return leftNode;
+    }
+
+    parseExpression(): ExpressionNode{
+        if(this.match(TokenTypeList.VARIABLE) === null){
+            const printNode = this.parsePrint();
+            return printNode;
+        }
+        this.position -= 1;
+        const variableNode = this.parseVariableOrNumber();
+        const assignOperator = this.match(TokenTypeList.ASSIGN);
+        if(assignOperator !== null){
+            const rightNode = this.parseFormula();
+            const binaryNode = new BinOperatorNode(assignOperator, variableNode, rightNode);
+            return binaryNode;
+        }
+
+        throw new Error(`После переменной ожидаеся оператор присвоения на позиции ${this.position}`)
+    }
+
+    parseCode(): ExpressionNode{
+        const root = new StatementsNode();
+        while(this.position < this.tokens.length){
+            const codeStringNode = this.parseExpression();
+            this.require(TokenTypeList.SEMICOLON);
+            root.addCode(codeStringNode);
+        }
+        return root;
+    }
+
+    run(node: ExpressionNode): any{
+        if(node instanceof NumberNode){
+            return parseInt(node.number.name);
+        }
+    }   
 }
